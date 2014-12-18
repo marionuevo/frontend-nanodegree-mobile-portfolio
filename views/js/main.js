4c91bf4681193837b3707965df111cf2f4616b4d
Mario Nuevo <marionuevo@gmail.com>
2014-12-18 12:21:13 +0000

    Cached document.body.scrollTop 'cos is unnecessary

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  // calling document.body.scrollTop is unnecesary on every iteration
+  var cachedScrollTop=document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((cachedScrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
